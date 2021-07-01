d1477f764c90340b013ee6034922f003ee9e59f9
nwelsh <71775175+nwelsh@users.noreply.github.com>
2021-07-01 08:18:47 -0500

    Create lesson2.cc

---
+++ lesson2.cc
@@ -0,0 +1,21 @@
+// https://www.learn-cpp.org/en/Variables_and_Types
+// 7/1 lesson: variables and types 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+enum colour {red, green, blue} a_colour, another_colour;
+//a_colour = green;  // a_colour will be assigned value of '1'
+
+//cout << a_colour
+
+int a = 1, b = 2, c = 3;
+a = a+b+c;
+
+  cout << "Goodbye, World!" << std::endl;
+    cout << a << std::endl;
+
+
+  return 0;
+}
